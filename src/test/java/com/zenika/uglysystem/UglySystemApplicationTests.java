64ddf0d263cb25fdd181facf4e3859348333a8f0
EC2 Default User <ec2-user@ip-172-31-3-3.eu-west-1.compute.internal>
2019-03-19 17:01:44 +0000

    fix test class

---
+++ src/test/java/com/zenika/uglysystem/UglySystemApplicationTests.java
@@ -0,0 +1,64 @@
+package com.zenika.uglysystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class UglySystemApplicationTests2 {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Test
+    public void contextLoads() {
+    }
+
+    /*
+    @Test
+    public void should_return_list_of_flags() throws Exception{
+        mvc.perform(MockMvcRequestBuilders.get("/flags")
+                .accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(content().string(FlagController.getAllFlags()))
+                .andDo(MockMvcResultHandlers.print());
+    }
+    */
+
+    @Test
+    public void should_return_error_message_for_admin_access() throws Exception{
+        mvc.perform(MockMvcRequestBuilders.get("/admin")
+                .accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(status().isUnauthorized())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @WithUserDetails("admin")
+    public void should_return_success_for_admin_access() throws Exception{
+        mvc.perform(MockMvcRequestBuilders.get("/admin")
+                .accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(content().string("SUCCESS"))
+                .andDo(MockMvcResultHandlers.print());
+    }
+}
