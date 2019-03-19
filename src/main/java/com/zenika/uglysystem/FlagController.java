3e87d88b12ff8a436e4f055b3de7c7ee0a86c6fa
Lorenz Hänggi <lolo8304@gmail.com>
2019-03-19 14:04:06 +0100

    updates flags

--- src/main/java/com/zenika/uglysystem/FlagController.java
+++ src/main/java/com/zenika/uglysystem/FlagController.java
@@ -27,7 +27,8 @@ public class FlagController {
                 "SECRET_8_DEV0_SEC2_OPS0_Jhd7vzV892DG7QAnefHc59Lr5L999wVDcYXar3Gvca9527UX",
                 "QUIZ_CONVERSATIONALAI_DEV1_SEC0_OPS0_h7s582j96p873h8c77mu2biy8sj62gkj8k8678y4yycc4k6f",
                 "WORKSHOP_DRMAD_DEV0_SEC2_OPS0_im5r86b85j8q9ct3q2238ix5r969744u3kmkk6g4tdfxh4d8",
-                "QUIZ_UITESTAUTOMATION_DEV1_SEC0_OPS0_kz7587pk975qhwdw78es99q6i9f879nf56u356v3tinp6n3t"
+                "QUIZ_UITESTAUTOMATION_DEV1_SEC0_OPS0_kz7587pk975qhwdw78es99q6i9f879nf56u356v3tinp6n3t",
+                "QUIZ_ZEROTOUCHPIAASTOPRODUCTION_DEV0_SEC0_OPS1_nd5i7an28ryru6bz3w6j737v89694k6uv67j5afa794ah7c3"
         );
     }
 
