4bdd4ce3f1a8fd20e4753f038abcaea9baed310b
EC2 Default User <ec2-user@ip-172-31-3-3.eu-west-1.compute.internal>
2019-03-19 09:06:08 +0000

    Add Sonar to Jenkiins Pipeline

--- src/main/java/com/zenika/uglysystem/FlagController.java
+++ src/main/java/com/zenika/uglysystem/FlagController.java
@@ -15,6 +15,11 @@ public class FlagController {
     @GetMapping
     public List<String> getFlags() {
         return asList(
+            "SECRET_9_DEV0_SEC0_OPS2_59S74TSc9P9dAP46Yc22FwG5ViDa8a6gW9aE8uAdpkd8WheW",
+            "SECRET_REDTEAMING_DEV0_SEC5_OPS0_kG4Q77PymVTe7Mm6NtNRkF3HuptQz93Ur6842bEw8Wz9T24iTo",
+            "SECRET_0_DEV5_SEC3-OPS4_2HcQaXfsHrFw3P2LD4JIY0nbd71Rsj07LX34jc6J9345mcAs",
+            "SECRET_5_DEV0_SEC0_OPS1_q5v9mGZ3WNkU329k9p675D455nn47vUGXFrnMLPwUbzMM9yi",
+            "QUIZ_BUILDINGANAGILEDATASCIENCE_DEV1_SEC0_OPS0_2h69x439hvx577kn4r2tk768wt3mgauc6em2u85t4q956f7s"
         );
     }
 
